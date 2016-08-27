f1c65657953daa5597bba19fb55012944c523227
Manfred Touron <m@42.am>
2016-08-27 11:27:14 +0200

    Add --interval=seconds flag

--- cmd/retry/main.go
+++ cmd/retry/main.go
@@ -5,11 +5,34 @@ import (
 	"os"
 	"os/exec"
 	"time"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "retry"
+	app.Author = "Manfred Touron"
+	app.Email = "https://github.com/moul/retry"
+	app.Version = "0.1.0"
+	app.Usage = "retry"
+
+	app.Flags = []cli.Flag{
+		cli.Float64Flag{
+			Name:   "interval, n",
+			Usage:  "seconds to wait between attempts",
+			Value:  1.0,
+			EnvVar: "RETRY_INTERVAL",
+		},
+	}
+
+	app.Action = retry
+	app.Run(os.Args)
+}
+
+func retry(c *cli.Context) error {
 	for {
-		cmd := exec.Command(os.Args[1], os.Args[2:]...)
+		cmd := exec.Command(c.Args()[0], c.Args()[1:]...)
 
 		cmd.Stderr = os.Stderr
 		cmd.Stdout = os.Stdout
@@ -25,6 +48,7 @@ func main() {
 		}
 
 		log.Printf("Command finished with error: %v", err)
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Duration(c.Float64("interval")*1000) * time.Millisecond)
 	}
+	return nil
 }
