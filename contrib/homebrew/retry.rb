eb1f8c0160012e17242f9f3cbee8de7afb2f0c80
Manfred Touron <m@42.am>
2016-12-09 12:54:59 +0100

    Bump homebrew @ v0.2.0

--- contrib/homebrew/retry.rb
+++ contrib/homebrew/retry.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Retry < Formula
   desc "retry: repeat shell commands"
   homepage "https://github.com/moul/retry"
-  url "https://github.com/moul/retry/archive/v0.1.0.tar.gz"
-  sha256 "b69819119d958f5dec197092b12c687b356e5bc12340b7ce03aeccd9c8854bb2"
+  url "https://github.com/moul/retry/archive/v0.2.0.tar.gz"
+  sha256 "1e69a41a99a64c3b49fc5588c733bd90a404c9a7132717a89f7569dee0512c9f"
 
   head "https://github.com/moul/retry.git"
 
